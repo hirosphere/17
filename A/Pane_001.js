75a578940f02761dd0ec06547dbf7025a1b285c2
hirosphere <umoof.x@gmail.com>
2017-10-15 23:27:33 +0900

    7

--- A/Pane_001.js
+++ A/Pane_001.js
@@ -6,13 +6,24 @@ var Pane_001 = class_def
 	{
 		this.Initiate = function( com )
 		{
-			var e = this.E = $.Create( "div", com, null, "Pane_001" );
-			var bu = $.Create( "button", e, null, "Push" );
 			var synth = new Syn_001.Synth();
+			
+			var e = this.E = $.Create( "div", com, null, "Pane_001" );
+			
+			chord_button( e, synth, -30, -18, -13, -11 );
+			chord_button( e, synth, -27, -15, -10, -8 );
+			chord_button( e, synth, -24, -12, -7, -5 );
+			chord_button( e, synth, -25, -13, -8, -6 );
+		};
+		
+		function chord_button( com, synth, b, c1, c2, c3 )
+		{
+			var bu = $.Create( "button", com, null, "Push" );
+			bu.style.padding = "1em";
 			
 			bu.onclick = function()
 			{
-				synth.SetChord( -30, -18, -13, -11 );
+				synth.SetChord( b, c1, c2, c3 );
 			};
 			
 			bu.onkeyup
@@ -21,6 +32,6 @@ var Pane_001 = class_def
 			{
 				synth.OffChord();
 			};
-		};
+		}
 	}
 );
